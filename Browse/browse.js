af73b47fbc8f87ef76fec099f8180ae3e7037c21
Nandini Kapure <nandinikapure2261@gmail.com>
2022-01-09 07:57:47 +0530

    updated files

---
+++ Browse/browse.js
@@ -0,0 +1,22 @@
+const nav = document.getElementById('nav');
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 100) {
+        nav.classList.add('nav__black');
+    } else {
+        nav.classList.remove('nav__black');
+    }
+});
+
+function home() {
+    window.location = "../index.html";
+}
+
+function open_modal() {
+    document.getElementsByClassName('modal')[0].style.display = "flex";
+    console.log();
+}
+
+
+function close_modal() {
+    document.getElementsByClassName('modal')[0].style.display = "none";
+}
